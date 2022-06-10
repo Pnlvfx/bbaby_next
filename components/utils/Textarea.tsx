2aa2db419b10cd664758afb2c05d2e89e4a7c137
PnlVFX <simone97_19@hotmail.it>
2022-06-10 17:05:48 +0200

    small fix

--- components/utils/Textarea.tsx
+++ components/utils/Textarea.tsx
@@ -1,4 +1,4 @@
-function Textarea (props) {
+function Textarea (props:any) {
     return (
         <textarea {...props} className={"bg-reddit_dark-brighter p-2 border border-reddit_dark-brightest rounded-md block "+ props.className} />
     );
