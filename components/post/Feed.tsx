1309d6af1d9e78b3941e820c9169a7a2623fbf81
simone <simonegauli@gmail.com>
2022-09-08 03:13:53 +0200

    new

--- components/post/Feed.tsx
+++ components/post/Feed.tsx
@@ -79,7 +79,7 @@ const Feed = ({ posts: ssrPost, community, author }: FeedProps) => {
             {process.env.NEXT_PUBLIC_NODE_ENV === 'production' && <GoogleAdsense2 />}
            <div>
             <InfiniteScroll
-                dataLength={posts?.length || 0}
+                dataLength={posts.length}
                 next={getMorePosts}
                 hasMore={true}
                 loader={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((_, idx) => (
@@ -88,7 +88,7 @@ const Feed = ({ posts: ssrPost, community, author }: FeedProps) => {
                 endMessage={<></>}
                 
               >
-                {posts?.map((post) => <Post key={post._id} post={post} isListing={true} />)}
+                {posts.map((post) => <Post key={post._id} post={post} isListing={true} />)}
               </InfiniteScroll>
            </div>
           </div>
