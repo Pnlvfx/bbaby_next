589dd2e43aa45824cc602b1edb8ab5ec2f3634f9
PnlVFX <simonegauli@gmail.com>
2022-09-28 22:06:57 +0200

    fix manifest.json

--- components/main/Layout.tsx
+++ components/main/Layout.tsx
@@ -48,11 +48,11 @@ const Layout = ({ children, error } : LayoutProps) => {
         <link rel='apple-touch-icon' sizes='144x144' href={`${siteUrl}/apple-touch-icon-144x144.png`} />
         <link rel='apple-touch-icon' sizes='152x152' href={`${siteUrl}/apple-touch-icon-152x152.png`} />
         <link rel='apple-touch-icon' sizes='180x180' href={`${siteUrl}/apple-touch-icon-180x180.png`} />
-        <link rel="icon" type='image/png' sizes='192x192' href={`/android-chrome-192x192.png`} />
-        <link rel="icon" type='image/png' sizes='32x32' href={`/favicon-32x32.png`} />
-        <link rel='icon' type='image/png' sizes='96x96' href='/android-icon-96x96.png' />
-        <link rel="icon" type='image/png' sizes='16x16' href={`/favicon-16x16.png`} />
-        <link rel='manifest' href='/manifest.json' />
+        <link rel="icon" type='image/png' sizes='192x192' href={`${siteUrl}/android-chrome-192x192.png`} />
+        <link rel="icon" type='image/png' sizes='32x32' href={`${siteUrl}/favicon-32x32.png`} />
+        <link rel='icon' type='image/png' sizes='96x96' href={`${siteUrl}/android-icon-96x96.png`} />
+        <link rel="icon" type='image/png' sizes='16x16' href={`${siteUrl}/favicon-16x16.png`} />
+        <link rel='manifest' href={`${siteUrl}/manifest.json'`} />
         <meta name='msapplication-TileColor' content='#ffffff' />
         <meta name='msapplication-TileImage' content='/mstile-150x150.png' />
         <meta name="theme-color" content="#1a1a1b" />
