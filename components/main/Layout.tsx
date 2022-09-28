5e96ca1c0559fa1a7cfc60fbfcdfc67b0b9bc827
PnlVFX <simonegauli@gmail.com>
2022-09-28 22:11:58 +0200

    fix

--- components/main/Layout.tsx
+++ components/main/Layout.tsx
@@ -52,7 +52,7 @@ const Layout = ({ children, error } : LayoutProps) => {
         <link rel="icon" type='image/png' sizes='32x32' href={`${siteUrl}/favicon-32x32.png`} />
         <link rel='icon' type='image/png' sizes='96x96' href={`${siteUrl}/android-icon-96x96.png`} />
         <link rel="icon" type='image/png' sizes='16x16' href={`${siteUrl}/favicon-16x16.png`} />
-        <link rel='manifest' href={`${siteUrl}/manifest.json'`} />
+        <link rel='manifest' href={`${siteUrl}/manifest.json`} />
         <meta name='msapplication-TileColor' content='#030303' />
         <meta name='msapplication-TileImage' content={`${siteUrl}/mstile-150x150.png`} />
         <meta name="theme-color" content="#1a1a1b" />
