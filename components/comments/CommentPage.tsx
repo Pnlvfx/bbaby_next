0b4cc3ad400b4dee95a0e8c1faee07af48f0955d
simone <simonegauli@gmail.com>
2023-02-02 03:02:57 +0100

    new

--- components/comments/CommentPage.tsx
+++ components/comments/CommentPage.tsx
@@ -12,13 +12,13 @@ const CommentPage = ({ post }: CommentPageProps) => {
   return (
     <>
       {session?.device?.mobile ? (
-        <div className="mb-2 box-border block bg-reddit_dark-brighter pt-2">
+        <div className="mb-2 bg-reddit_dark-brighter pt-2">
           <Comment post={post} />
         </div>
       ) : (
         <div className="flex min-h-[calc(100vh_-_48px)] flex-col">
-          <div className="mx-auto box-border flex max-w-[1600px] flex-row justify-center md:py-5 md:px-6">
-            <div className="w-full flex-none break-words rounded-md bg-reddit_dark-brighter lg:mr-4 lg:w-7/12 xl:w-5/12 2xl:w-[750px]">
+          <div className="flex max-w-[1600px] flex-row justify-center md:py-5 md:px-6 lg:mx-auto">
+            <div className="w-full max-w-[750px] flex-none break-words rounded-md bg-reddit_dark-brighter lg:mr-4 lg:w-7/12 xl:w-8/12 2xl:w-[750px]">
               <Comment post={post} />
             </div>
             {!session?.device?.mobile && (
